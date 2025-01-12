a0b694a270bb4c16d42fb1c0c753a07e3ab12976
PranavKulkarni09 <mr.pranav.kulkarni.s@gmail.com>
2025-01-12 03:52:37 -0600

    Create sipo_register.sv

---
+++ Day_30/sipo_register.sv
@@ -0,0 +1,11 @@
+module sipo_register(clk, serial_in, out);
+    input logic clk, serial_in;
+  output logic[3:0] out=0;
+    always @(posedge clk)
+    begin
+      out[3]<=serial_in;
+      out[2]<=out[3];
+      out[1]<=out[2];
+      out[0]<=out[1];
+    end 
+endmodule
