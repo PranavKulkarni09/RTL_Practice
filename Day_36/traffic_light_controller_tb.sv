dbc61fa9d9bbf84d5593168c52b0623da0e6e1bc
PranavKulkarni09 <mr.pranav.kulkarni.s@gmail.com>
2025-02-01 23:39:35 -0600

    Update traffic_light_controller_tb.sv

--- Day_36/traffic_light_controller_tb.sv
+++ Day_36/traffic_light_controller_tb.sv
@@ -10,12 +10,10 @@ module traffic_light_controller_tb();
         clk = 0;
         rst = 1;
         #10 rst = 0;
-
         #100 $finish;
     end
 
     initial begin
-      $monitor("Time=%0t | Vehicle_at_R=%b | Vehicle_at_Y=%b | Vehicle_at_G=%b | Ped_at_R=%b Ped_at_G=%b", 
-                 $time, red, yellow, green, ped_red, ped_green);
+        $monitor("Time=%0t | Vehicle_at_R=%b | Vehicle_at_Y=%b | Vehicle_at_G=%b | Ped_at_R=%b Ped_at_G=%b", $time, red, yellow, green, ped_red, ped_green);
     end
 endmodule
