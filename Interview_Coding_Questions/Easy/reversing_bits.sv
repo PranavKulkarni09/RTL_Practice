2544bb0077d71ba7d497e7ac0cec4cbbf296431f
PranavKulkarni09 <mr.pranav.kulkarni.s@gmail.com>
2026-01-02 10:26:45 -0800

    Create reversing_bits.sv

---
+++ Interview_Coding_Questions/Easy/reversing_bits.sv
@@ -0,0 +1,14 @@
+module model #(parameter
+  DATA_WIDTH=32
+) (
+  input  [DATA_WIDTH-1:0] din,
+  output logic [DATA_WIDTH-1:0] dout
+);
+
+  always@(*) begin
+    for(int i = 0; i<DATA_WIDTH; i++) begin
+      dout[i] = din[DATA_WIDTH-1-i];
+    end
+  end
+
+endmodule
